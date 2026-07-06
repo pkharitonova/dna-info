e4d972e752d1e3919267f0696142ac13d8fbac71
Polina Kharitonova <thntvavs@gmail.com>
2026-07-07 01:59:58 +0300

    feat: initial version of DNA Info

---
+++ main.py
@@ -0,0 +1,9 @@
+title = "=== DNA INFO ==="
+print(title)
+print()
+print("Enter the DNA sequence:")
+dna_seq = input()
+print("-" * len(title))
+print("Sequence:", dna_seq)
+dna_length = len(dna_seq) 
+print("Length:", dna_length)
